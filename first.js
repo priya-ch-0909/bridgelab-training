5aedf84b525bf751aafef0999f73a5e1b7e23b51
Priya chaudhary <priya-ch-0909>
2026-01-15 14:27:32 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,8 @@
+console.log("node js backend started");
+ 
+we can chnage the value in let. it is block Scoped. we can reddeclare it.
+const is used when u dont need to change the value . it is also block scope.
+string is the collection of chahracters
+common operation are length , concatenation Etc..
+number is the data type that store the value of integer and decimals 
+constant is the variabe for which value cant be changed .it is block scope variable
