314d090608137816b4b0eaa58d9bf4715cfff6bb
Priya chaudhary <priya-ch-0909>
2026-01-15 15:14:47 +0530

    second commit

--- first.js
+++ first.js
@@ -6,3 +6,16 @@ string is the collection of chahracters
 common operation are length , concatenation Etc..
 number is the data type that store the value of integer and decimals 
 constant is the variabe for which value cant be changed .it is block scope variable
+object is like a dictionary it has key and vlaue pair.it uses curly braces for its notation 
+it does have object access like person.name
++ is used for addition
+- is used for subtraction
+* is used for multiplication
+/ is used for division
+  if is the sattement whenever it happens then another statment will get happen 
+  else is alternative of if
+there are differnet type of loops for iteration for loop is used and while loop is used as per condition . 
+do while loop is used when we want to make one condition pass then other conditons are applied in loop.
+  function is a blosk of code in which we can reuse it multiple times it takes input and return output
+
+
